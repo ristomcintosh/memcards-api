c193abc1fd9c1c23d8455538823f0a7208d43353
Risto Mcintosh <risto.mcintosh@gmail.com>
2024-07-15 19:13:40 -0500

    initial go api

---
+++ go/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", whatUpHandler)
+
+	fmt.Println("severing on port 5757")
+	log.Fatal(http.ListenAndServe(":5757", r))
+}
+
+func whatUpHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("What up"))
+}
